e65f3fa74e33f249e26cdb7c2e27ecb28ff487da
avvie <avatar.of.virtues@googlemail.com>
2018-01-13 12:50:54 +0200

    First Commit on the repo

---
+++ Finger.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Finger {
+	public int id;
+	public TouchPhase phase;
+	public Vector2 screenPosition, lastScreenPosition;
+	public Vector3 worldPosition, lastWorldPosition, Wdirection;
+	public Vector2 Sdirection;
+	public Finger(int d) {
+		id = d;
+	}
+
+	public Finger(int d, TouchPhase p, Vector2 Spos, Vector3 Wpos) {
+		id = d;
+		phase = p;
+		screenPosition = Spos;
+		worldPosition = Wpos;
+		//2d Only
+		worldPosition.z = 0;
+	}
+
+	public void MoveFinger(Touch t) {
+		phase = t.phase;
+		lastScreenPosition = screenPosition;
+		screenPosition = t.position;
+		lastWorldPosition = worldPosition;
+		worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+		
+		//2d Only
+		worldPosition.z = 0;
+
+		if (t.phase == TouchPhase.Moved) {
+			Sdirection = screenPosition - lastScreenPosition;
+			Wdirection = worldPosition - lastWorldPosition;
+		}
+		else {
+			Sdirection = Vector2.zero;
+			Wdirection = Vector3.zero;
+		}
+
+	}
+}
---
+++ FingerManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class FingerManager {
+	List<Finger> fingers = new List<Finger>();
+	public int Count = 0;
+
+
+	Finger linearSearchId(int id) {
+		foreach(Finger finger in fingers) {
+			if (finger.id == id)
+				return finger;
+		}
+		return new Finger(-1);
+	}
+
+	public void delete(int id) {
+		fingers.Remove(linearSearchId(id));
+	}
+
+	public void addFinger(Touch touch) {
+		if(touch.phase == TouchPhase.Began) {
+			fingers.Add(new Finger(touch.fingerId, 
+				touch.phase, 
+				touch.position, 
+				Camera.main.ScreenToWorldPoint(touch.position)));
+		}
+		Count++;
+	}
+
+	public bool touchExists(Touch touch, bool addToList) {
+		if (addToList) {
+			if (touch.phase == TouchPhase.Began) {
+				addFinger(touch);
+				return false;
+			}
+
+			Finger temp = linearSearchId(touch.fingerId);
+			if (temp.id == -1) {
+				addFinger(touch);
+				return true;
+			}
+			return false;
+		}
+		else {
+			Finger temp = linearSearchId(touch.fingerId);
+			if (temp.id == -1) {
+				return true;
+			}
+			return false;
+		}
+	}
+
+	public List<Finger> ClearFingers() {
+		fingers.Clear();
+		Count = 0;
+		return fingers;
+	}
+
+	public List<Finger> updateList(Touch[] touches) {
+		Finger temp;
+		foreach (Touch t in touches) {
+			//Debug.Log(t.phase);
+			if (t.phase == TouchPhase.Canceled || t.phase == TouchPhase.Ended)
+				delete(t.fingerId);
+
+			if (t.phase == TouchPhase.Began)
+				touchExists(t, true);
+
+			if(t.phase == TouchPhase.Moved || t.phase == TouchPhase.Stationary) {
+				temp = linearSearchId(t.fingerId);
+				temp.MoveFinger(t);
+			}
+
+		}
+		return fingers;
+	}
+
+}
